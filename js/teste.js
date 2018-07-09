ed21b4aed6e158b15f3860962a962dc63c9a9f61
Joao <joaovcacador@gmail.com>
2018-07-09 01:56:14 -0300

    modal222

---
+++ js/teste.js
@@ -0,0 +1,17 @@
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+/*alert();
+alert("John");
+var firstName = "John";
+firstName;
+alert(42);
+14 + 28;
+alert(firstName + " is awesome!");*/
